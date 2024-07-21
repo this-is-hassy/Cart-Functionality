53c2d273a7d0cf15e15ad01d112360036bc84b35
Hassan Hameed <126436517+this-is-hassy@users.noreply.github.com>
2024-07-21 16:58:56 +0500

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -28,7 +28,7 @@ const Item = ({ title, price }) => {
                       onClick={() =>
                         cart.setItem([...cart.item, { title, price }])
                       }
-                      className="flex mx-auto mt-2 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                      className="flex mx-auto  text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                     >
                       Add to Cart
                     </button>
